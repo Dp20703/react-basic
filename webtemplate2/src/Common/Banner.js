6ff6c33b5d0a1244640dca4a82c586109b718f6b
dp <codewithdp2073@gmail.com>
2025-01-02 18:38:32 +0530

    webtemplate2 added

---
+++ webtemplate2/src/Common/Banner.js
@@ -0,0 +1,18 @@
+export default function Banner(){
+    return (
+        <>
+            <section class="w3l-about-breadcrumb">
+                <div class="breadcrumb-bg breadcrumb-bg-about py-sm-5 pt-5 pb-4">
+                    <div class="container pt-lg-5 pt-md-3 py-lg-4 pb-md-3">
+                        <h2 class="title">About Us</h2>
+                        <ul class="breadcrumbs-custom-path mt-2">
+                            <li><a href="index.html">Home</a></li>
+                            <li class="mx-2">/ </li>
+                            <li class="active"> About Us </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
---
+++ webtemplate2/src/Common/SubService.js
@@ -0,0 +1,72 @@
+export default function SubService() {
+    return (
+        <>
+            <section class="w3l-servicesblock1 py-5" id="moreservices">
+                <div class="container py-lg-5 py-md-4 py-3">
+                    <div class="grids-area-hny main-cont-wthree-fea row ">
+                        <div class="col-lg-4 col-md-6 grids-feature">
+                            <div class="area-box">
+                                <span class="fa fa-retweet mt-0"></span>
+                                <div class="area-box-info">
+                                    <h4><a href="#feature" class="title-head">Social Media Strategy</a></h4>
+                                    <p class="mt-lg-3 mt-2">Vivamus a ligula quam tesque et libero justo, ultrices in. Ut eu leo non. Duis
+                                        sed et dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 grids-feature mt-md-0 mt-5">
+                            <div class="area-box">
+                                <span class="fa fa-clock-o mt-0"></span>
+                                <div class="area-box-info">
+                                    <h4><a href="#feature" class="title-head">Branding Strategy</a></h4>
+                                    <p class="mt-lg-3 mt-2">Vivamus a ligula quam tesque et libero justo, ultrices in. Ut eu leo non. Duis
+                                        sed et dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 grids-feature mt-lg-0 mt-5">
+                            <div class="area-box">
+                                <span class="fa fa-line-chart mt-0"></span>
+                                <div class="area-box-info">
+                                    <h4><a href="#feature" class="title-head">UI/UX Design</a></h4>
+                                    <p class="mt-lg-3 mt-2">Vivamus a ligula quam tesque et libero justo, ultrices in. Ut eu leo non. Duis
+                                        sed et dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 grids-feature mt-5 pt-lg-4">
+                            <div class="area-box">
+                                <span class="fa fa-trash-o mt-0"></span>
+                                <div class="area-box-info">
+                                    <h4><a href="#feature" class="title-head">Digital Product Strategy</a></h4>
+                                    <p class="mt-lg-3 mt-2">Vivamus a ligula quam tesque et libero justo, ultrices in. Ut eu leo non. Duis
+                                        sed et dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 grids-feature mt-5 pt-lg-4">
+                            <div class="area-box">
+                                <span class="fa fa-video-camera mt-0"></span>
+                                <div class="area-box-info">
+                                    <h4><a href="#feature" class="title-head">Web Hosting Service</a></h4>
+                                    <p class="mt-lg-3 mt-2">Vivamus a ligula quam tesque et libero justo, ultrices in. Ut eu leo non. Duis
+                                        sed et dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 grids-feature mt-5 pt-lg-4">
+                            <div class="area-box">
+                                <span class="fa fa-search-plus mt-0"></span>
+                                <div class="area-box-info">
+                                    <h4><a href="#feature" class="title-head">SEO Services</a></h4>
+                                    <p class="mt-lg-3 mt-2">Vivamus a ligula quam tesque et libero justo, ultrices in. Ut eu leo non. Duis
+                                        sed et dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
